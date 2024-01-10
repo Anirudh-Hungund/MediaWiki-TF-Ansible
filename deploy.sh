43481d3254addcc8d24211cc8f3e1a7de788d15f
anirudh.hungund <anirudh.hungund@outlook.com>
2024-01-10 10:01:31 +0530

    Changed Deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,10 @@ set -eux
 terraform init
 terraform apply -auto-approve
 
+
+ansible-galaxy collection install cloud.terraform
+ansible-galaxy collection install community.mysql
+ansible-galaxy collection install ansible.posix
+
 ansible-playbook -i inventory.yml ./Ansible/playbook.yml
 
